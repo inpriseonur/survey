377b6ce91f90330d1556ba00c12930363042b8a1
Onur <35793288+inpriseonur@users.noreply.github.com>
2023-09-23 09:20:51 +0300

    destop ilk commit

--- survey_dash.py
+++ survey_dash.py
@@ -3,6 +3,16 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import seaborn as sns
+
+st.set_page_config(layout='wide')
+
+st.title(':blue[Chicago Crime] Database :cop:')
+st.caption('streamlit experiments')
+st.divider()
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(40, text=progress_text)
+
 
 
 def survey_data_load():
@@ -54,18 +64,18 @@ def calculate_data (df):
 
 def calculate_data2 (df):
     df_supports =df[df['Answer'] > 8] #find supports
-    df_calculate = df_supports.groupby(['Question','Departmant'], as_index = False).size()
+    df_calculate = df_supports.groupby(['Question','Departmant', 'S Month'], as_index= False).size()
     df_calculate['Supporters'] = df_calculate['size']
 
     df_notr= df[(df['Answer'] >6) & (df['Answer'] <9)] #find notr
-    df_notr = df_notr.groupby(['Question','Departmant'], as_index= False).size() 
+    df_notr = df_notr.groupby(['Question','Departmant', 'S Month'], as_index= False).size()
     df_calculate['Notur'] = df_notr['size']
 
     df_detractors =df[df['Answer'] < 7] #find detractors
-    df_detractors = df_detractors.groupby(['Question','Departmant'], as_index= False).size()
+    df_detractors = df_detractors.groupby(['Question','Departmant', 'S Month'], as_index= False).size()
     df_calculate['Detractors'] = df_detractors ['size']
 
-    df_total_count = df.groupby(['Question','Departmant'], as_index = False).size() #sum partisipated employee of survey 
+    df_total_count = df.groupby(['Question','Departmant', 'S Month'], as_index = False).size() #sum partisipated employee of survey 
     df_calculate['SUM'] = df_total_count['size']
 
     suports_yuzdesi = df_calculate['Supporters'] / (df_calculate['SUM'] / 100)
@@ -76,8 +86,20 @@ def calculate_data2 (df):
 
     detractors_yuzdesi = df_calculate['Detractors'] / (df_calculate['SUM'] / 100)
     df_calculate['Detractors %'] = round(detractors_yuzdesi,2)
+    df_calculate.sort_values (by='Departmant')
 
     return df_calculate
+
+
+def calculate_data3 (df):
+    df_supports = df[df['Answer'] > 8] #fin
+    #df_supports = df_supports.groupby(['Departmant','Question']).size().unstack(fill_value=0).stack()
+    #df_supports = df_supports.groupby([pd.Categorical(df_supports.Departmant), 'Question']).size().fillna(0)
+    df_supports = df_supports.groupby(['Question', 'Departmant']).Answer.count().reindex(df_supports['Departmant'].unique()).fillna(0).astype(int).rename('a_count').reset_index()
+    st.data_editor(df_supports)
+
+
+
 
 
 def analysis_data (df):
@@ -89,15 +111,21 @@ df = survey_data_load() #first data load
 df = create_sidebar (df) # creating sidebar objects and data
 #df = analysis_data (df)
 df_calc = calculate_data2(df)
+#calculate_data3(df)
+
+
+#my_bar = st.progress(supporter_count, text=progress_text)
+
+     
+df_calc = df_calc.sort_values(['Departmant'], ascending=True)
 st.data_editor(df_calc) 
 
 investment_by_business_type= df_calc
-st.write (investment_by_business_type)
 
 fig_investment=px.bar(
     investment_by_business_type,
     x="Detractors %",
-    y="Departmant",
+    y="Question",
     orientation="h",
     title="<b> Investment by Business Type </b>",
     color_discrete_sequence=["#0083B8"]*len(investment_by_business_type),
@@ -112,3 +140,14 @@ xaxis=(dict(showgrid=False))
 
 left,right,center=st.columns(3)
 right.plotly_chart(fig_investment,use_container_width=True)
+left.plotly_chart(fig_investment,use_container_width=True)
+
+
+
+
+
+
+
+with st.container():
+   #st.plotly_chart(fig_investment,use_container_width=True)
+   st.write("This is inside the container")
